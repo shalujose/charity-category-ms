2c9460dbc79fea0ff951c21fd94aa102b3cc9e62
shalujose <shalujosem0@gmail.com>
2019-10-29 19:25:36 +0530

    Delete category changes committed

--- src/main/java/com/revature/charityappcategoryms/controller/CategoryController.java
+++ src/main/java/com/revature/charityappcategoryms/controller/CategoryController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -71,12 +73,12 @@ public class CategoryController {
 
 	}
 	
-	@PostMapping()
+	@DeleteMapping("{id}")
 	@ApiOperation(value = "categoryAPI")
 	@ApiResponses(value = { @ApiResponse(code = 201, message = "Category deleted", response = String.class),
 			@ApiResponse(code = 400, message = "Invalid Category", response = Category.class) })
 	
-	public ResponseEntity<?> deleteByCategory( int categoryId) throws ServiceException {
+	public ResponseEntity<?> deleteByCategory(@PathVariable("id") Integer categoryId) throws ServiceException {
 
 		try {
 			categoryService.deleteCategory(categoryId);
--- src/main/java/com/revature/charityappcategoryms/repository/CategoryRepository.java
+++ src/main/java/com/revature/charityappcategoryms/repository/CategoryRepository.java
@@ -1,7 +1,9 @@
 package com.revature.charityappcategoryms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.revature.charityappcategoryms.model.Category;
@@ -9,7 +11,8 @@ import com.revature.charityappcategoryms.model.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-	@Query("form Category DELETE where id = :id")
-	void deleteById(int id);
+	@Modifying
+	@Query("DELETE from Category where id = :id")
+	void deleteById(@Param("id") int id);
 	
 }
